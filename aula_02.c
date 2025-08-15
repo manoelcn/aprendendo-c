d30915b3fdad2c947c91e542d15e3831e52dce0b
Manoel <candidomanoel78@gmail.com>
2025-08-15 10:11:54 -0300

    aula 02

---
+++ aula_02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Comentário em uma linha
+
+    /*
+    Comentário em mais de uma linha
+    */
+
+    printf("Hello, world!\n");
+    // \n quebra linha
+
+    return 0;
+}
