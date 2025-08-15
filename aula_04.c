9476ceaa8a974a57cfed7dc4dd4fd5104d5cd701
Manoel <candidomanoel78@gmail.com>
2025-08-15 10:34:25 -0300

    aula 04

---
+++ aula_04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Como criar uma variável:
+    int minhaIdade;
+    minhaIdade = 19;
+
+    int maeIdade;
+    maeIdade = 50;
+
+    // Outra forma de criar uma variável:
+    int paiIdade = 52;
+
+    // Como imprimir uma variável:
+    printf("Minhha idade é %i.\nPai idade %i.\nMãe idade %i.\n",
+        minhaIdade, paiIdade, maeIdade);
+
+    return 0;
+}
