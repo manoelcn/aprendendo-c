a3031609e4689f677514d4e16e4b225047b48d18
Manoel <candidomanoel78@gmail.com>
2025-08-15 10:21:41 -0300

    aula 03

---
+++ aula_03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Como criar uma variável:
+    int minhaIdade;
+    minhaIdade = 19;
+
+    // Como imprimir uma variável:
+    printf("Minha idade é %i.\n", minhaIdade);
+    return 0;
+}
