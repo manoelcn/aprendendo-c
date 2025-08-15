11d98a44ab26b166027657602704ce20946ec027
Manoel <candidomanoel78@gmail.com>
2025-08-15 10:47:25 -0300

    aula 05

---
+++ aulas/aula_05.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int minhaIdade = 10;
+    int maeIdade = 50;
+    int paiIdade = 55;
+    int irmaoIdade = 15;
+
+    int idadeTotal;
+    idadeTotal = minhaIdade + maeIdade + paiIdade + irmaoIdade;
+
+    printf("A soma das idades é %i.\n", idadeTotal);
+
+    return 0;
+}
